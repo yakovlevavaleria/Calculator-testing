249e5148c372f670c24f753e2db337b97e5bacb1
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-18 01:57:02 +0300

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -1,60 +1,52 @@
-double sum(double a, double b)
+double sum ( float a,  float b)
 {
 	return a + b;
 }
-double subtraction(double a, double b)
+double sub ( float a,  float b)
 {
 	return a - b;
 }
-double multiplication(double a, double b)
+double mult ( float a,  float b)
 {
-	return a*b;
+	return a * b;
 }
-double division(double a, double b)
+double div ( float a,  float b)
 {
-	return a / b;
+	return (float)a / b;
 }
-double power(double a, double b)
+double pow ( float a )
 {
-
-	double c = 1;
-
-	if (b>0)
-
-		for (int i = 0; i < b; i++)
-
-			c = c*a;
-
-	if (b<0)
-
-		for (int i = 0; i < (-b); i++)
-
-			c = c / a;
-
-	return c;
+	unsigned int extent = 4;
+	float a2 = a;
+	
+	if ( extent != 0 ) 
+	{
+			for (int i = 1; i < extent; i++)
+		{
+			a = a * a2;
+		}
+	}
+		else a = 1.0;
+	
+	return (double) a;
 }
-double squareroot(double a)
+double sq ( float a )
 {
+	float xn = 1.0;  
+	float xn1 = 2.0;
 
-	double i = 0;
+	if ( a != 1 && a != 0 )
+	{
+		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
+		{
+		 xn = xn1;
+		 xn1 = (float)(xn + (float)a/xn)/2;
+		}
 
-	while (i*i < a)
-		i = i + 0.000001;
+	}
+		else if ( a == 1 ) xn1 = 1;
+			else if ( a == 0 ) xn1 = 0;
 
-	return i;
-}
-double absolut(double a)
-{
 
-	return ((a >= 0) ? a : -a);
-}
-
-double roundp(double a)
-{
-
-	if (a < 0)
-
-		a = -absolut(a);
-
-	return a;
+	return xn1;
 }
