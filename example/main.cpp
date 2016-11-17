e3c924bd99a9add4841bfec685fb45ce7723f926
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-18 01:55:14 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,92 +1,20 @@
-#include "stdafx.h"
 #include <iostream>
 
+#include "calculator.hpp"
 using namespace std;
-
-double sum(double a, double b)
-{
-	return a + b;
-}
-double subtraction(double a, double b)
-{
-	return a - b;
-}
-double multiplication(double a, double b)
-{
-	return a*b;
-}
-double division(double a, double b)
-{
-	return a / b;
-}
-double power(double a, double b)
-{
-
-	double c = 1;
-
-	if (b>0)
-
-		for (int i = 0; i < b; i++)
-
-			c = c*a;
-
-	if (b<0)
-
-		for (int i = 0; i < (-b); i++)
-
-			c = c / a;
-
-	return c;
-}
-double squareroot(double a)
-{
-
-	double i = 0;
-
-	while (i*i < a)
-		i = i + 0.000001;
-
-	return i;
-}
-double absolut(double a)
-{
-
-	return ((a >= 0) ? a : -a);
-}
-
-double roundp(double a)
-{
-
-	if (a < 0)
-
-		a = -absolut(a);
-
-	return a;
-}
-
 
 int main()
 {
-	int a, b, c;
-	setlocale(LC_ALL, "rus");
-	cout << "Введите число a=";
-	cin >> a;
-	cout << "Введите число b=";
-	cin >> b;
-	cout << "Введите число c=";
-	cin >> c;
-	cout << "Сумма чисел a+b= " << roundp(sum(a, b)) << endl;
-	cout << "Разность чисел a-b= " << roundp(subtraction(a, b)) << endl;
-	cout << "Произведение чисел a*b= " << roundp(multiplication(a, b)) << endl;
-	cout << "Честное чисел a/b= " << roundp(division(a, b)) << endl;
-	cout << "Возведение в степень a^b= " << roundp(power(a, b)) << endl;
-	if (a >= 0)
-
-		cout << "Корень из a " << squareroot(a) << endl;
-
-	else
-
-		cout << "Корень извлечь нельзя" << endl;
-	system("pause");
-
+    float a, b;
+    cin >> a;
+    cin >> b;
+    
+    cout << sum( a, b ) << endl;
+    cout << sub( a, b ) << endl;
+    cout << mult( a, b ) << endl;
+    cout << div( a, b ) << endl; 
+    cout << pow( a ) << endl;
+    cout << sq( a ) << endl;
+    
+    return 0;
 }
