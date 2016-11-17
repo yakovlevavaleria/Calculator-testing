836134edd9c85209b04402e6bfca8a29bf725954
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-17 22:56:45 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,1 +1,10 @@
+double sum(double a, double b)
+double subtraction(double a, double b)
+double multiplication(double a, double b)
+double division(double a, double b)
+double power(double a, double b)
+	double c = 1;
+	if (b>0)
+		for (int i = 0; i < b; i++)
+			c = c*a;
 
