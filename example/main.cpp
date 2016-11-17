cfec6cf1591a6060d7b6e1b3b2292e2add562cbd
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-18 00:57:50 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,27 +1,92 @@
+#include "stdafx.h"
 #include <iostream>
 
-#include "calculator.hpp"
 using namespace std;
+
+double sum(double a, double b)
+{
+	return a + b;
+}
+double subtraction(double a, double b)
+{
+	return a - b;
+}
+double multiplication(double a, double b)
+{
+	return a*b;
+}
+double division(double a, double b)
+{
+	return a / b;
+}
+double power(double a, double b)
+{
+
+	double c = 1;
+
+	if (b>0)
+
+		for (int i = 0; i < b; i++)
+
+			c = c*a;
+
+	if (b<0)
+
+		for (int i = 0; i < (-b); i++)
+
+			c = c / a;
+
+	return c;
+}
+double squareroot(double a)
+{
+
+	double i = 0;
+
+	while (i*i < a)
+		i = i + 0.000001;
+
+	return i;
+}
+double absolut(double a)
+{
+
+	return ((a >= 0) ? a : -a);
+}
+
+double roundp(double a)
+{
+
+	if (a < 0)
+
+		a = -absolut(a);
+
+	return a;
+}
 
 
 int main()
 {
-	double a, b;	
-	int c;	
-        cin >> a;		
-        cin >> b;	       	
-        cin >> c;	
-        cout << roundp(sumary(a, b)) << endl;	
-        cout << roundp(differense(a, b)) << endl;	
-        cout << roundp(multiplication(a, b)) << endl;
-	if (b==0)
-		cout << "Na nol delit nelzya" << endl;
-	else	
-                cout << roundp(quotient(a, b)) << endl;	
-        cout << roundp(power(a, c)) << endl;	
-        if (a >= 0)		
-                cout << squareroot(a) << endl;	
-        else		
-                cout << "koren izvlech nelzya" << endl;	
-return 0;
+	int a, b, c;
+	setlocale(LC_ALL, "rus");
+	cout << "Введите число a=";
+	cin >> a;
+	cout << "Введите число b=";
+	cin >> b;
+	cout << "Введите число c=";
+	cin >> c;
+	cout << "Сумма чисел a+b= " << roundp(sum(a, b)) << endl;
+	cout << "Разность чисел a-b= " << roundp(subtraction(a, b)) << endl;
+	cout << "Произведение чисел a*b= " << roundp(multiplication(a, b)) << endl;
+	cout << "Честное чисел a/b= " << roundp(division(a, b)) << endl;
+	cout << "Возведение в степень a^b= " << roundp(power(a, b)) << endl;
+	if (a >= 0)
+
+		cout << "Корень из a " << squareroot(a) << endl;
+
+	else
+
+		cout << "Корень извлечь нельзя" << endl;
+	system("pause");
+
 }
