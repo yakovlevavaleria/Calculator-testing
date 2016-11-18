b2e0aa0b87b26449e014c17fc42feb85e83dd338
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-18 10:50:42 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -3,18 +3,25 @@
 #include "calculator.hpp"
 using namespace std;
 
+
 int main()
 {
-    float a, b;
-    cin >> a;
-    cin >> b;
-    
-    cout << sum( a, b ) << endl;
-    cout << sub( a, b ) << endl;
-    cout << mult( a, b ) << endl;
-    cout << div( a, b ) << endl; 
-    cout << pow( a ) << endl;
-    cout << sq( a ) << endl;
-    
-    return 0;
+	double a, b;	
+	int c;	
+        cin >> a;		
+        cin >> b;	       	
+        cin >> c;	
+        cout << roundp(sumary(a, b)) << endl;	
+        cout << roundp(differense(a, b)) << endl;	
+        cout << roundp(multiplication(a, b)) << endl;
+	if (b==0)
+		cout << "Na nol delit nelzya" << endl;
+	else	
+                cout << roundp(quotient(a, b)) << endl;	
+        cout << roundp(power(a, c)) << endl;	
+        if (a >= 0)		
+                cout << squareroot(a) << endl;	
+        else		
+                cout << "koren izvlech nelzya" << endl;	
+return 0;
 }
