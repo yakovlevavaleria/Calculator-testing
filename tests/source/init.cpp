9c886de2e66a67b5418de6bef7ae075736e473fc
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-18 11:16:29 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,56 +2,56 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sumary", "[sumary]") {
-    double a = 16.0;
-    double b = 8.0;
+    double a = 25.0;
+    double b = 1.0;
     int c = 3;
     
     double rv = sumary( a, b );
     
-	REQUIRE( rv == 24.0 );
+	REQUIRE( rv == 26.0 );
 }
 SCENARIO("calculator differense", "[differense]") {
-    double a = 16.0;
-    double b = 8.0;
+    double a = 25.0;
+    double b = 1.0;
     int c = 3;
     
     double rv = differense( a, b );
     
-	REQUIRE( rv == 8.0 );
+	REQUIRE( rv == 24.0 );
 }
 SCENARIO("calculator multiplication", "[multiplication]") {
-    double a = 16.0;
-    double b = 8.0;
+    double a = 25.0;
+    double b = 1.0;
     int c = 3;
     
     double rv = multiplication( a, b );
     
-	REQUIRE( rv == 128.0 );
+	REQUIRE( rv == 25.0 );
 }
 SCENARIO("calculator quotient", "[quotient]") {
-    double a = 16.0;
-    double b = 8.0;
+    double a = 25.0;
+    double b = 1.0;
     int c = 3;	
     
     double rv = quotient( a, b );
     
-	REQUIRE( rv == 2.0 );
+	REQUIRE( rv == 25.0 );
 }
 SCENARIO("calculator power", "[power]") {
-    double a = 16.0;
-    double b = 8.0;
-    int c = 3;
+    double a = 25.0;
+    double b = 1.0;
+    int c = 2;
 	
     double rv = power( a, c );
     
-	REQUIRE( rv == 4096.0 );
+	REQUIRE( rv == 625.0 );
 }
 SCENARIO("calculator squareroot", "[squareroot]") {
-    double a = 16.0;
-    double b = 8.0;
+    double a = 25.0;
+    double b = 1.0;
     int c = 3;	
     
     double rv = squareroot( a );
     	
-REQUIRE( rv == 4.0 );
+REQUIRE( rv == 5.0 );
 }
