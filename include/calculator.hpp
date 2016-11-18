26d5c67b68a85362a852bc9857ea403849177a4f
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-11-18 10:51:32 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,6 +1,8 @@
-double sum ( float a,  float b);
-double sub ( float a,  float b);
-double mult ( float a,  float b);
-double div ( float a,  float b);
-double pow ( float a );
-double sq ( float a );
+double sumary(double a, double b);
+double differense(double a, double b);
+double multiplication(double a, double b);
+double quotient(double a, double b);
+double power(double a, int c);
+double squareroot(double a);
+double absolut(double a);
+double roundp(double a);
