8b1c2a69a3dc8d03c305a415a721cbbc6d43da50
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-09 15:00:00 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -19,7 +19,10 @@ int main() {
     cout << "Умножение " << Mult(a, b)<< endl;
     cout << "Разность " << Sub(a, b)<< endl;
     cout << "Степень " << Pow1(a, b)<< endl;
-    cout << "Корень " << Sqrt3(a)<< endl;
+    double *r = Sqrt3(a);
+	if (r) cout << "a^1/2=" << *Sqrt3(a) << endl;
+	else cout << "a < 0 -> error" << endl;
+	delete r;
     }
     else cout << "Неправильный ввод";
     system("pause");
