c59d1cfa0c49b3cc7f8dc40b0287054cfa3b91f7
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-09 13:57:10 +0400

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
-
 #include "calculator.hpp"
+
 using namespace std;
 
-
-int main()
-{
-	double a, b;	
-	int c;	
-        cin >> a;		
-        cin >> b;	       	
-        cin >> c;	
-        cout << roundp(sumary(a, b)) << endl;	
-        cout << roundp(differense(a, b)) << endl;	
-        cout << roundp(multiplication(a, b)) << endl;
-	if (b==0)
-		cout << "Na nol delit nelzya" << endl;
-	else	
-                cout << roundp(quotient(a, b)) << endl;	
-        cout << roundp(power(a, c)) << endl;	
-        if (a >= 0)		
-                cout << squareroot(a) << endl;	
-        else		
-                cout << "koren izvlech nelzya" << endl;	
-return 0;
+int main() {
+    double a;
+    unsigned int b;
+    cin >> a;
+    cin >> b;
+    if(cin){
+    cout << "Cумма " << Sum(a, b)<< endl;
+    long double *buf = Dev(a,b);
+    if (buf) cout << "Частное: " << *buf << endl;
+    else cout << "На ноль делить нельзя." << endl;
+    cout << "Умножение " << Mult(a, b)<< endl;
+    cout << "Разность " << Sub(a, b)<< endl;
+    cout << "Степень " << Pow1(a, b)<< endl;
+    cout << "Корень " << Sqrt3(a)<< endl;
+    }
+    else cout << "Неправильный ввод";
+    system("pause");
 }
