4160caf934821bd42f7b6059b8a0c3e28a1af097
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-09 15:00:33 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -3,4 +3,4 @@ long double *Dev(double x,double y);
 long double Mult(double x,double y);
 long double Sub(double x,double y);
 long double Pow1(double x,double y);
-long double Sqrt3(double x);
+long double *Sqrt3(double x);
