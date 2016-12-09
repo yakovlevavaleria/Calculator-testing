2425b2349cb221b8697512502ba28af36ce8e8fe
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-09 14:27:29 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -10,9 +10,12 @@ int main() {
     cin >> b;
     if(cin){
     cout << "Cумма " << Sum(a, b)<< endl;
-    long double *buf = Dev(a,b);
-    if (buf) cout << "Частное: " << *buf << endl;
-    else cout << "На ноль делить нельзя." << endl;
+    long double *buf = dev(a, b);
+	if (buf!=nullptr)
+		cout << "a / b = " << *buf << endl;
+	else
+		cout << "a/0 -> error" << endl;
+	delete buf;
     cout << "Умножение " << Mult(a, b)<< endl;
     cout << "Разность " << Sub(a, b)<< endl;
     cout << "Степень " << Pow1(a, b)<< endl;
