1d740ea10ffa2c96b81eb05f91967742556f072a
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-16 03:40:47 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,44 +1,30 @@
-#include <iostream> 
+#include <iostream>
+
 #include "calculator.hpp"
 using namespace std;
 
-int main() {
-	int a, b;
-	cout << "Enter a,b:";
-	cin >>a;
-	cin >>b;
-	cout << "Sum:" << sum(a, b) << endl;
-	cout << "Dev:" << dev(a, b) << endl;
-	cout << "Mult:" << mult(a, b) << endl;
-	try {
-		cout <<"Sub:"<<sub(a,b)<<endl;
-	}
-	catch (char *Err)
-	{
-		cout << Err << endl;
-	}
-   try {
-		cout << "Pow a to b: " << pow(a, b) << endl;
-	}
-	catch (int i)
-	{
-		cout << "Pow a to b:" << i << endl;
-	}
-	try{
-	cout << "Sqrt a: " << sqrta(a) << endl;
-	}
-	catch (char *Errr)
-	{
-		cout<<Errr<<endl;
-	}
-	try{
-	cout << "Sqrt b: " << sqrta(a) << endl;
-	}
-	catch (char *Errrr)
-	{
-		cout<<Errrr<<endl;
-	}
 
+int main()
+{
+	double a, b;	
+	int c;	
+        cin >> a;		
+        cin >> b;	       	
+        cin >> c;	
+        cout << roundp(sumary(a, b)) << endl;	
+        cout << roundp(differense(a, b)) << endl;	
+        cout << roundp(multiplication(a, b)) << endl;
+	double * res = quotient(a, b);
+	if (res!=nullptr)
+		cout << "a / b = " << * res << endl;
+	else
+		cout << "a/0 -> error" << endl;
+	delete res;
+	cout << roundp(power(a, c)) << endl;	
+	double *r = squareroot(a);
+	if (r) cout << "a^0.5=" << *squareroot(a) << endl;
+	else cout << "a < 0 -> error" << endl;
+	delete r;
+	
 	return 0;
-
 }
