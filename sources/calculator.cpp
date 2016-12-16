804af78ed4f1507de18cea47344b686eceb809f3
yakovlevavaleria <yakovlevavaleri9@gmail.com>
2016-12-16 03:42:44 +0300

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -1,72 +1,64 @@
-double sum(int a, int b) {
-	return a+b;
+ouble sumary(double a, double b)
+{	
+        return a + b;
 }
-
-double dev(int a, int b) {
-	return a-b;
-
+double differense(double a, double b)
+{	
+        return a - b;
 }
-
-double mult(int a, int b) {
-	return a*b;
-
+double multiplication(double a, double b)
+{	
+        return a * b;
 }
-
-double sub(int a, int b) {
-     if(b == 0)
-     {
-       throw "Error.Impossible to divide by zero";
-     }
-	return a/b;
-
-}
-
-double pow(int a, int b) {
-        if(b==0)
+double * quotient(double a, double b)
+{	
+	if (b!=0)
 	{
-	throw 1;
+	        double *p = new double;
+		*p = a / b;
+		return p;
 	}
-	
-	unsigned int i = 1;
-	if(b<0){
-		double pow=1.;
-		for(i=1;i<b;++i){
-		pow/=(double)a;	
-		}
-	}
-	else{
-		double pow = a;
-	for (i = 1; i<b; i++) {
-		pow *=(double) a;
-
-	}}
-	return pow;
+	else return nullptr;
 }
-
-double sqrta(int a) {
-	if(a<0)
-	{
-	throw "Impossible"
-	}
-	unsigned int div = 1, rslt = 0;
-	while (a> 0)
-	{
-		a -= div, div += 2;
-		rslt += a < 0 ? 0 : 1;
-	}
-	return rslt;
-
+double power(double a, int c)
+{	
+        double a1 = 1;	
+        if (c>0)		
+                for (int i = 0; i < c; i++)		
+                        a1 = a1*a;	
+        if (c<0)	
+                for (int i = 0; i < (-c); i++)			
+                        a1 = a1 / a;
+        return a1;
 }
-double sqrtb(int b) {
-	if(b<0)
+double *squareroot(double a)
+{ 
+	double c = 0, res = a;
+        if (a > 0)
 	{
-	throw "Impossible"
+	       double *r = new double;
+	       if (a != 1 && a != 0)
+	       {
+	            while (c - res > 0.00001 || c - res < -0.00001)
+		    {
+		         c = res;
+		         res = (c + a / res) / 2;
+		    }
+	            *r = res;
+	       }
+	       else if (a == 1) *r = 1;
+     	       else if (a == 0) *r = 0;
+	return r;	
 	}
-	unsigned int div1 = 1, rslt1 = 0;
-	while (b> 0)
-	{
-		b -= div1, div1 += 2;
-		rslt1 += b < 0 ? 0 : 1;
-	}
-	return rslt1;
+	else return nullptr;
+}	
+double absolut(double a)
+{	
+        return ((a >= 0) ? a : -a);
+}
+double roundp(double a)
+{	
+        if (a < 0)
+                a = -absolut(a);
+        return a;
 }
